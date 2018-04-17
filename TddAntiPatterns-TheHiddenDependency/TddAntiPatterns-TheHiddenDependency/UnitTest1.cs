ecac9cd1270b728023b2319e9a22afe710eeee86
Matheus Rodrigues <matheus.moreira.rodrigues01@gmail.com>
2018-04-17 20:51:45 -0300

    Creating the project

---
+++ TddAntiPatterns-TheHiddenDependency/TddAntiPatterns-TheHiddenDependency/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace TddAntiPatterns_TheHiddenDependency
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
